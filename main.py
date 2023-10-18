e4edf29c295778a480914af286c7938ca4ece554
Noodles <thisabhijithere@protonmail.com>
2023-10-18 22:54:44 +0530

    Need a lot of fixes about the various returntypes as well as a lot of methods are missing. Need to first make a barebones model.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Spotify:
     def __init__(self) -> None:
         pass
-    def setupConnection(self, id, secret) -> bool:
+    def setupConnection(self, id, secret):
         
         # Set up authentication
         auth_url = 'https://accounts.spotify.com/api/token'
@@ -19,15 +19,73 @@ class Spotify:
         auth_token = auth_response.json()['access_token']
         self.headers = {"Authorization": f"Bearer {auth_token}"}
 
-    def fetchPlaylistsData(self, link):
-        pass
-        
+    def fetchPlaylistsData(self, link, need = ['all']):
+        response = requests.get(link, headers=self.headers)
+
+        if response.status_code == 200:
+            print(response)
+            data = response.json()
+            if need[0] == 'all':
+                return data
+            else:
+                result = list()
+                for needed in need:
+                    if needed == 'name' or needed == 'description' or needed == 'tracks':
+                        result.append(data[needed])
+                    fields = needed.split('/')
+                    if fields[0] == 'tracks':
+                        if fields[1] == 'total':
+                            result.append(data['tracks']['total'])
+                        if fields[1] == 'items':
+                            '''Too much useless data, need to filter it'''
+                            result.append(data['tracks']['items'])
+                return result             
+
+    def fetchTrackData(self, link, need = ['all']):
+        response = requests.get(link, headers=self.headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            if need[0] == 'all':
+                return data
+            else:
+                ''' make it happen
+                result = list()
+                for needed in need:
+                    if needed == 'name' or needed == 'description' or needed == 'tracks':
+                        result.append(data[needed])
+                    fields = needed.split('/')
+                    if fields[0] == 'tracks':
+                        if fields[1] == 'total':
+                            result.append(data['tracks']['total'])
+                        if fields[1] == 'items':
+                            ''''''Too much useless data, need to filter it''''''
+                            result.append(data['tracks']['items'])'''
+                return 
+    
+    def saveTrack(self, track_id):
+        response = requests.put(f'https://api.spotify.com/v1/me/tracks?ids={track_id}', headers=self.headers)
+        return response
+    
+    def removeTrack(self, track_id):
+        response = requests.delete(f'https://api.spotify.com/v1/me/tracks?ids={track_id}', headers=self.headers)
+        return response
+    
+    def followPlaylist(self, playlist_id):
+        response = requests.post(f'https://api.spotify.com/v1/playlists/{playlist_id}/followers', headers=self.headers)
+        return response
+    
+    def unfollowPlaylist(self, playlist_id):
+        response = requests.delete(f'https://api.spotify.com/v1/playlists/{playlist_id}/followers', headers=self.headers)
+        return response
+    
 
 
-link = 'https://open.spotify.com/playlist/7tm08i6ITfMB3itnUbeSV2?si=6e10edd4a6ec4ff5'
+link = 'https://api.spotify.com/v1/playlists/7tm08i6ITfMB3itnUbeSV2?si=6e10edd4a6ec4ff5'
 isPlaylist = lambda link : True if 'playlist' in link else False
 id = os.environ.get('id')
 secret = os.environ.get('secret')
 x=Spotify()
 print(x.setupConnection(id, secret))
 print(isPlaylist(link))
+print(x.fetchPlaylistsData(link=link, need=['name', 'tracks/total', 'tracks/items']))
